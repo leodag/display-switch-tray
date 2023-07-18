7814b8c27cdb336395047fbcf84410faa9794e49
Leonardo Dagnino <leodag.sch@gmail.com>
2023-07-18 20:45:46 -0300

    feat: detect failure

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,142 @@
 use gtk::glib;
+use gtk::glib::SignalHandlerId;
 use gtk::prelude::*;
+use std::path::Path;
 use std::process::Command;
+use std::cell::RefCell;
+use std::rc::Rc;
 use appindicator3::prelude::*;
 use appindicator3::{Indicator, IndicatorStatus, IndicatorCategory};
 
 const APP_NAME: &str = "Display Switch Tray";
 
+#[derive(Debug)]
+enum ActiveStateEnum {
+    Inactive,
+    Active,
+}
+
+#[derive(Debug)]
+enum FailedStateEnum {
+    Failed,
+    Normal,
+}
+
+#[derive(Debug)]
+struct ServiceState {
+    active: ActiveStateEnum,
+    failed: FailedStateEnum,
+}
+
+impl ServiceState {
+    fn new(active_state: ActiveStateEnum, failed_state: FailedStateEnum) -> Self {
+        ServiceState { active: active_state, failed: failed_state }
+    }
+
+    fn sync_state(&mut self, active_state: ActiveStateEnum, failed_state: FailedStateEnum) {
+        self.active = active_state;
+        self.failed = failed_state;
+    }
+
+    fn change_state(&mut self, desired_state: ActiveStateEnum) -> FailedStateEnum {
+        let success = match desired_state {
+            ActiveStateEnum::Active => {
+                let status = Command::new("sudo")
+                    .args(["systemctl", "start", "display_switch.service"])
+                    .status()
+                    .expect("failed to start");
+
+                status.code().unwrap_or(1) == 0
+            },
+            ActiveStateEnum::Inactive => {
+                let status = Command::new("sudo")
+                    .args(["systemctl", "stop", "display_switch.service"])
+                    .status()
+                    .expect("failed to stop");
+
+                status.code().unwrap_or(1) == 0
+            }
+        };
+
+        if success {
+            self.active = desired_state;
+            self.failed = FailedStateEnum::Normal;
+            FailedStateEnum::Normal
+        } else {
+            self.failed = FailedStateEnum::Failed;
+            FailedStateEnum::Failed
+        }
+    }
+}
+
+fn current_unit_state() -> (ActiveStateEnum, FailedStateEnum) {
+    let active = Command::new("systemctl")
+        .args(["is-active", "--quiet", "display_switch.service"])
+        .status()
+        .expect("failed to check active");
+
+    let failed = Command::new("systemctl")
+        .args(["is-failed", "--quiet", "display_switch.service"])
+        .status()
+        .expect("failed to check failed");
+
+    let active_state = match active.code() {
+        Some(0) => ActiveStateEnum::Active,
+        _ => ActiveStateEnum::Inactive,
+    };
+
+    let failed_state = match failed.code() {
+        Some(0) => FailedStateEnum::Failed,
+        _ => FailedStateEnum::Normal,
+    };
+
+    (active_state, failed_state)
+}
+
+fn set_icon(indicator: &Indicator, service_state: &ServiceState) {
+    match service_state.failed {
+        FailedStateEnum::Failed => {
+            indicator.set_status(IndicatorStatus::Attention);
+        },
+        FailedStateEnum::Normal => {
+            match service_state.active {
+                ActiveStateEnum::Active => {
+                    indicator.set_icon_full("active-symbolic", "icon");
+                    indicator.set_status(IndicatorStatus::Active);
+                },
+                ActiveStateEnum::Inactive => {
+                    indicator.set_icon_full("inactive-symbolic", "icon");
+                    indicator.set_status(IndicatorStatus::Active);
+                }
+            }
+        }
+    }
+}
+
 fn main() {
     gtk::init().unwrap();
 
+    let icon_path = Path::new(env!("CARGO_MANIFEST_DIR"));
+
     let indicator = Indicator::builder(APP_NAME)
         .category(IndicatorCategory::ApplicationStatus)
-        // .menu(&m)
-        .icon("computer-symbolic", "icon")
-        .status(IndicatorStatus::Passive)
+        .icon_theme_path(icon_path.to_str().unwrap())
+        .icon("active-symbolic", "icon")
+        .attention_icon("action-unavailable-symbolic", "failed")
+        .status(IndicatorStatus::Active)
         .title(APP_NAME)
         .label("Display Switch")
-        // .secondary_activate_target(&mi_enabled)
         .build();
+
+    let (active_state, failed_state) = current_unit_state();
+    let state = ServiceState::new(active_state, failed_state);
+    set_icon(&indicator, &state);
+
+    let service_state = Rc::new(RefCell::new(state));
 
     let m = gtk::Menu::new();
 
-    let mi_enabled = gtk::CheckMenuItem::with_label("Enabled");
-    mi_enabled.set_active(true);
+    let mi_enabled = gtk::CheckMenuItem::with_label("Active");
     m.append(&mi_enabled);
 
     let sep = gtk::SeparatorMenuItem::new();
@@ -32,25 +146,39 @@ fn main() {
     m.append(&mi_quit);
     m.show_all();
 
-    mi_enabled.connect_activate(glib::clone!(@weak indicator => move |mi_enabled| {
+    let handler_id: Rc<RefCell<Option<SignalHandlerId>>> = Rc::new(RefCell::new(None));
+
+    let h_id = mi_enabled.connect_activate(glib::clone!(@strong handler_id, @weak indicator, @strong service_state => move |mi_enabled| {
         if mi_enabled.is_active() {
-            let cmd = Command::new("sudo")
-                .args(["systemctl", "start", "display_switch"])
-                .status()
-                .expect("failed to start");
+            let mut state = service_state.as_ref().borrow_mut();
 
-            indicator.set_icon_full("computer-symbolic", "icon");
-            println!("start status {}", cmd.code().unwrap());
+            match state.change_state(ActiveStateEnum::Active) {
+                FailedStateEnum::Failed => {
+                    mi_enabled.block_signal(&handler_id.as_ref().borrow_mut().as_mut().unwrap());
+                    mi_enabled.set_active(false);
+                    mi_enabled.unblock_signal(&handler_id.as_ref().borrow_mut().as_mut().unwrap());
+                }
+                _ => (),
+            }
+
+            set_icon(&indicator, &state);
         } else {
-            let cmd = Command::new("sudo")
-                .args(["systemctl", "stop", "display_switch"])
-                .status()
-                .expect("failed to stop");
+            let mut state = service_state.as_ref().borrow_mut();
 
-            indicator.set_icon_full("action-unavailable-symbolic", "icon");
-            println!("stop status {}", cmd.code().unwrap());
+            match state.change_state(ActiveStateEnum::Active) {
+                FailedStateEnum::Failed => {
+                    mi_enabled.block_signal(&handler_id.as_ref().borrow_mut().as_mut().unwrap());
+                    mi_enabled.set_active(false);
+                    mi_enabled.unblock_signal(&handler_id.as_ref().borrow_mut().as_mut().unwrap());
+                }
+                _ => (),
+            }
+
+            set_icon(&indicator, &state);
         }
     }));
+
+    *handler_id.as_ref().borrow_mut() = Some(h_id);
 
     mi_quit.connect_activate(|_| {
         gtk::main_quit();
@@ -59,7 +187,6 @@ fn main() {
     indicator.set_menu(Some(&m));
 
     indicator.set_secondary_activate_target(Some(&mi_enabled));
-    indicator.set_status(IndicatorStatus::Active);
 
     gtk::main();
 
